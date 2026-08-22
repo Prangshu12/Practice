21f4f40ddf1b46bd0064e0550d372f4d255f7d14
Prangshu12 <prangshu12@gmail.com>
2026-08-22 08:52:03 +0530

    second commit

---
+++ logic.js
@@ -0,0 +1,33 @@
+// const Student_name = "Prangshu Pranjal Saikia";
+// let age = 18;
+// let course = "BTech Cse"
+// var College_name = "SITM";
+// let Marks = 80 + "%";
+// const Birth_year = 2008;
+// age = 19;
+// Marks = 80.5 + "%";
+// let City = "Golaghat";
+// let Semester = 1 + "st";
+// let Favourite_subject = "coding";
+
+// console.log("Student name: " + Student_name);
+// console.log("Student age: " + age);
+// console.log("Student course: " + course);
+// console.log("College name: " + College_name);
+// console.log("Student's perfomance: " + Marks);
+// console.log("Birth year: " + Birth_year);
+// console.log("City: " + City);
+// console.log("Semester: " + Semester);
+// console.log("Student's subject preference: " + Favourite_subject);
+
+// //This is the place where the error will occur
+// // Birth_year = 2007; 
+// console.log(Birth_year);
+// let arr= ["apple", "banana", "orange"];
+// console.log(arr[0]);
+
+let n = 10;
+n += 5;
+console.log(n);
+n *= 2;
+console.log(n);
