bd7578e74ac78c71ede2e5b4ce05c2ba26628e50
Prangshu12 <prangshu12@gmail.com>
2026-08-25 15:13:55 +0530

    4th

--- logic.js
+++ logic.js
@@ -1,33 +1,64 @@
-// const Student_name = "Prangshu Pranjal Saikia";
-// let age = 18;
-// let course = "BTech Cse"
-// var College_name = "SITM";
-// let Marks = 80 + "%";
-// const Birth_year = 2008;
-// age = 19;
-// Marks = 80.5 + "%";
-// let City = "Golaghat";
-// let Semester = 1 + "st";
-// let Favourite_subject = "coding";
+"use strict";
+// // const Student_name = "Prangshu Pranjal Saikia";
+// // let age = 18;
+// // let course = "BTech Cse"
+// // var College_name = "SITM";
+// // let Marks = 80 + "%";
+// // const Birth_year = 2008;
+// // age = 19;
+// // Marks = 80.5 + "%";
+// // let City = "Golaghat";
+// // let Semester = 1 + "st";
+// // let Favourite_subject = "coding";
 
-// console.log("Student name: " + Student_name);
-// console.log("Student age: " + age);
-// console.log("Student course: " + course);
-// console.log("College name: " + College_name);
-// console.log("Student's perfomance: " + Marks);
-// console.log("Birth year: " + Birth_year);
-// console.log("City: " + City);
-// console.log("Semester: " + Semester);
-// console.log("Student's subject preference: " + Favourite_subject);
+// // console.log("Student name: " + Student_name);
+// // console.log("Student age: " + age);
+// // console.log("Student course: " + course);
+// // console.log("College name: " + College_name);
+// // console.log("Student's perfomance: " + Marks);
+// // console.log("Birth year: " + Birth_year);
+// // console.log("City: " + City);
+// // console.log("Semester: " + Semester);
+// // console.log("Student's subject preference: " + Favourite_subject);
 
-// //This is the place where the error will occur
-// // Birth_year = 2007; 
-// console.log(Birth_year);
-// let arr= ["apple", "banana", "orange"];
-// console.log(arr[0]);
+// // //This is the place where the error will occur
+// // // Birth_year = 2007; 
+// // console.log(Birth_year);
+// // let arr= ["apple", "banana", "orange"];
+// // console.log(arr[0]);
 
-let n = 10;
-n += 5;
-console.log(n);
-n *= 2;
-console.log(n);
+// let n = 10;
+// n += 5;
+// console.log(n);
+// n *= 2;
+// console.log(n);
+// m = 10
+// m %= 3;
+// console.log(m);
+// let age = 0;
+
+
+let age = 0;
+const readline = require("readline");
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log("Welcome to the voting eligibility checker!");
+input.question("Enter your age: ", (age) => {
+    age = parseInt(age);
+    if (age> 18){
+    console.log("You are eligible to vote.");
+}else if (age == 18) {
+    console.log("CONGRATULATIONS! YOU CAN VOTE NOW.");
+}
+ else {
+    console.log("You are not eligible to vote.");
+}
+    input.close();
+});
+
+// for (let i = 1; i <= 24; i++) {
+//     age = i;
+//     console.log(age);
--- python-first.py
+++ python-first.py
@@ -1,45 +1,96 @@
-from pyparsing import nums
+# # # # from pyparsing import nums
 
 
-print("hello world")
-x = 10
-print(x)
-y = "hello"
-print(y)
-print("datatype of x is: " + str(type(x)))
-print("datatype of y is: " + str(type(y)))
+# # # # print("hello world")
+# # # # x = 10
+# # # # print(x)
+# # # # y = "hello"
+# # # # print(y)
+# # # # print("datatype of x is: " + str(type(x)))
+# # # # print("datatype of y is: " + str(type(y)))
 
-img_numb = 2 + 3j
-print(img_numb)
-print(type(img_numb))
-a = [1,2,3,4]
-print(a)
-b = [1, "alasdjf", 2,3]
-print(b)
-print(type(b))
-# aklj ;asd fasd;l f asdlfk;j asdlk 
-fruits = ["apple", "orange", "banana"]
-print(fruits)
-fruits.append("grapes")
-print(fruits)
-point = (1,2,3)
-print(point)
-print(type(point))
-nums = {1,2,3,4,5}
-print(type(nums))
+# # # # img_numb = 2 + 3j
+# # # # print(img_numb)
+# # # # print(type(img_numb))
+# # # # a = [1,2,3,4]
+# # # # print(a)
+# # # # b = [1, "alasdjf", 2,3]
+# # # # print(b)
+# # # # print(type(b))
+# # # # # aklj ;asd fasd;l f asdlfk;j asdlk 
+# # # # fruits = ["apple", "orange", "banana"]
+# # # # print(fruits)
+# # # # fruits.append("grapes")
+# # # # print(fruits)
+# # # # point = (1,2,3)
+# # # # print(point)
+# # # # print(type(point))
+# # # # nums = {1,2,3,4,5}
+# # # # print(type(nums))
 
-name = "Prangshu"
-greet = "hello " + name
-print(greet)
-print(greet[0:15])
+# # # # name = "Prangshu"
+# # # # greet = "hello " + name
+# # # # print(greet)
+# # # # print(greet[0:15])
 
-student = {
-    "name": "Prangshu",
-    "age": 20,
-    "grade": "A"
-}
-print(student)
-print(student["name"])
-student["age"] = 21
-print(student)
-Full_name = "Prangshu Pranjal Saikia"
+# # # # student = {
+# # # #     "name": "Prangshu",
+# # # #     "age": 20,
+# # # #     "grade": "A"
+# # # # }
+# # # # print(student)
+# # # # print(student["name"])
+# # # # student["age"] = 21
+# # # # print(student)
+# # # # Full_name = "Prangshu"
+
+# # # import math
+# # # radius = 5
+# # # def area(r):
+# # #     area = math.pi * r * r
+# # #     return area
+# # # print(area(radius))
+
+
+# # # # for fruit in ["apple", "banana", "cherry"]:
+# # # #     print(fruit)
+
+# # for j in range(1, 7):
+# #     print("*" * 6)
+
+# # for i in range(1, 7):
+# #     print("*" * (i+0))
+
+    
+# # for i in range(1, 7):
+# #     print(" " * (6 - i) + "*" * (2 * i - 1))
+
+# score = 0
+# for i in range(1, 101):
+#     score = i
+#     print(score)
+
+# if score >= 90:
+#     print("Grade: A")
+# elif score >= 80:
+#     print("Grade: B")
+# elif score >= 70:
+#     print("Grade: C")
+# elif score >= 60:
+#     print("Grade: D")
+# elif score >= 50:
+#     print("Grade: E")
+# else:
+#     print("Grade: F")
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print(number, "is an even number")
+# else:
+#     print(number, "is an odd number")
+
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
